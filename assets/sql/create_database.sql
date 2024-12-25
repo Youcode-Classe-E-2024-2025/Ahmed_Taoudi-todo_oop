c2f3650f080d8089aff4154d5eb3aef4aff20e0f
Tawdi <ahmedtawdi00@gmail.com>
2024-12-25 16:00:48 +0100

    structurer le projet ajoute de code sql de creation de tables

---
+++ assets/sql/create_database.sql
@@ -0,0 +1,46 @@
+/* drop database if exists rento_car ; */ 
+create database if not exists taskflow ;
+
+/*  */
+
+use taskflow ;
+
+/*  table user  */
+
+create table if not exists user (
+    id int AUTO_INCREMENT PRIMARY KEY ,
+    username VARCHAR(255) not null ,
+    useremail VARCHAR(255) unique not null,
+    userpassword text not null,
+    date_inscription timestamp DEFAULT CURRENT_TIMESTAMP 
+);
+
+/*  table task  */
+
+create table if not exists task (
+    id int AUTO_INCREMENT PRIMARY KEY ,
+    taskname VARCHAR(50) not null ,
+    taskdesc VARCHAR(50) not null ,
+    taskstatus ENUM('TODO', 'DOING', 'DONE') NOT NULL DEFAULT 'TODO' ,
+    taskstart timestamp   ,
+    taskfin timestamp   
+);
+
+/*  table task_type  */
+
+create table if not exists task_type (
+task_id INT not null ,
+type enum('bug','feature')
+);
+
+/*  table collaboration  */
+
+create table if not exists collaboration (
+    user_id  INT  not null,
+    task_id  INT  not null,
+    primary key (user_id , task_id) ,
+    foreign key (user_id) references user(id),
+    foreign key (task_id) references task(id)
+);
+
+
---
+++ models/Database.php
@@ -0,0 +1,78 @@
+<?php
+require_once("assets/configDB.php");
+define('HOST', $host);
+define('PORT', $port);
+define('DBNAME', $dbname);
+define('USER', $user);
+define('PASSWORD', $password);
+
+// dd($dsn);
+class Database
+{
+    public $connection;
+    private  $dsn;
+
+
+    public function __construct()
+    {
+        $this->dsn = "mysql:host=" . HOST . ";port=" . PORT . ";dbname=" . DBNAME;
+
+        try {
+
+            $this->connection = new PDO($this->dsn, USER, PASSWORD);
+
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+
+            if ($e->getCode() == 1049) {
+                echo " info : The database does not exist.<br>";
+                echo '<form action=""  method="POST">
+                        <button type="submit" name="create_db">Create Database</button>
+                      </form>';
+            } else {
+                // Catch other PDO exceptions
+                echo "Connection failed: " . $e->getMessage();
+            }
+        }
+    }
+
+    public function query($query,$param = [])
+    {
+        try {
+            $statement = $this->connection->prepare($query);
+            $statement->execute($param);
+            return $statement;
+        } catch (PDOException $e) {
+
+            echo "Query failed: " . $e->getMessage();
+
+        }
+    }
+public function test(){
+    echo("in database class");
+}
+    public function createDatabase($dbname) {
+        try {
+            
+            $filePath ="assets/database/create_database.sql";
+            
+            if (!file_exists($filePath)) {
+                echo "SQL file not found.";
+                return;
+            }
+
+            $this->dsn = "mysql:host=" . HOST . ";port=" . PORT;
+            $this->connection = new PDO($this->dsn, USER, PASSWORD);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+           
+            $query = file_get_contents($filePath);
+            $this->connection->exec($query);
+            echo "Database '$dbname' created successfully.";
+            header("Location: /");
+        } catch (PDOException $e) {
+            echo "Error creating database: " . $e->getMessage();
+        }
+    }
+}
+
