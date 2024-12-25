2c6e0f97792a56270b1baac86cc47860ed4a860b
Tawdi <ahmedtawdi00@gmail.com>
2024-12-25 23:55:27 +0100

    initial route

--- models/Router.php
+++ models/Router.php
@@ -1,5 +1,19 @@
 <?php 
 
 class Router {
-    
+
+    protected $routes = []; 
+     
+    public function get($uri,$ctl){
+
+    }
+    public function post($uri,$ctl){
+        
+    }
+    public function delete($uri,$ctl){
+        
+    }
+    public function update($uri,$ctl){
+        
+    }
 }
