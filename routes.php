16bd34e42caba1470e3289dddd60a4f7b046f3f9
Tawdi <ahmedtawdi00@gmail.com>
2024-12-28 15:02:05 +0100

    modifier les routes

--- routes.php
+++ routes.php
@@ -2,23 +2,23 @@
 
 // GET
 
-$router->get('/','controllers/pages/index.ctl.php');
-$router->get('/profile','controllers/pages/profile.ctl.php');
-$router->get('/task','controllers/task/taskController.php');
+$router->get('/','controllers/pages/index.ctl.php','Index');
+$router->get('/profile','controllers/user/UserController.php','UserController');
+$router->get('/task','controllers/task/taskController.php','TaskController');
 
-$router->get('/login','controllers/connection/login.ctl.php');
-$router->get('/signup','controllers/connection/signup.ctl.php');
+// $router->get('/login','controllers/connection/login.ctl.php');:
+// $router->get('/signup','controllers/connection/signup.ctl.php');
 
 //  POST
 
-$router->post('/login','controllers/login.ctl.php');
-$router->post('/signup','controllers/signup.ctl.php');
+// $router->post('/login','controllers/login.ctl.php');
+// $router->post('/signup','controllers/signup.ctl.php','UserController');
 
 //  UPDATE
 
-$router->update('/task','controllers/task/taskController.php');
+// $router->update('/task','controllers/task/TaskController.php','taskController');
 
 
 //  DELETE
 
-$router->delete('/task','controllers/task/taskController.php');
+// $router->delete('/task','controllers/task/TaskController.php','taskController');
