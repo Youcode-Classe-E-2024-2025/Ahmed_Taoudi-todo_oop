0bec93580b086b6d6a8014c4edfbfe67a2cb0ce5
Tawdi <ahmedtawdi00@gmail.com>
2024-12-29 01:15:11 +0100

    ajoute login  signup

--- controllers/user/UserController.php
+++ controllers/user/UserController.php
@@ -4,18 +4,18 @@ require_once "models/Validator.php";
 require_once "models/User.php";
 class UserController
 {
-    private $conn ;
+    private $conn;
     public function __construct($db)
     {
-        $this->conn = $db ;
+        $this->conn = $db;
     }
     // Show user profile (GET)
     public function show()
     {
-        if (isset($_SESSION['userid']) && isset($_SESSION['username'])){
+        if (isset($_SESSION['userid']) && isset($_SESSION['username'])) {
 
             require_once "views/profile.view.php";
-        }else{
+        } else {
             require_once "views/auth.php";
         }
     }
@@ -23,32 +23,38 @@ class UserController
     // Register a new user (POST)
     public function store()
     {
-         // CSRF
-         if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        // CSRF
+        if (isset($_SESSION['username'])) {
+            header('location: /');
+            return;
+        }
+        if (isset($_POST['login'])) {
+            return $this->login();
+        }
+        if (isset($_POST['logout'])) {
+            return $this->logout();
+        }
+        if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             die("CSRF token validation failed. Possible CSRF attack.");
         }
         // dd($_POST);
         $useremail = Validator::XSS($_POST['email']);
         //  dd( $emailExist);
         $username = Validator::XSS($_POST['username']);
-        $emailExists = User::isIndatabase($useremail , $this->conn);
+        $emailExists = User::isIndatabase($useremail, $this->conn);
         $password = $_POST['password'];
         // dd($emailExists);
-        if ($emailExists === 0) {
+        if ($emailExists > 0) {
             echo "you can not use this email try another one <br>";
             require("views/auth.php");
             return;
+        } else {
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+            $newUser = new User('', $useremail, $username, $hashedPassword);
+            $newUserId = $newUser->createUser($this->conn);
+            // dd($newUserId);
+            require("views/auth.php");
         }
-        // } else {
-        //     $hashedPassword = password_hash($password, PASSWORD_BCRYPT); 
-        //     $newUser = $db->query(
-        //         "insert into utilisateur (email,name,mot_pass) values (:email, :name , :password)",
-        //         ['name' => $username, 'email' => $useremail, 'password' => $hashedPassword ]
-        //     );
-        //     $id = $db->query("select id from utilisateur where email = :email", ['email' => $useremail])->fetchColumn();
-        //     $db->query("insert into user_status (utilisateur_id) values (:id)",['id'=>$id]);
-        //     require("views/login.view.php");
-        // }
         echo "User registration process...";
         require_once "views/profile.view.php";
     }
@@ -56,18 +62,49 @@ class UserController
     // Log the user in (POST)
     public function login()
     {
-         // CSRF
-         if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        if (isset($_SESSION['username'])) {
+            header('location: /');
+            return;
+        }
+        // CSRF
+        if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             die("CSRF token validation failed. Possible CSRF attack.");
         }
-        echo "User login process...";
+        // dd($_POST);
+        $useremail = $_POST['email'];
+        $password = $_POST['password'];
+        $emailExists = User::isIndatabase($useremail, $this->conn);
+        if ($emailExists > 0) {
+            $loginUser = new User($this->conn, $useremail);
+            if ($loginUser->checkPassword($password)) {
+                // dd([$loginUser->getName(),$loginUser->getId()]);
+                $_SESSION['username'] = $loginUser->getName();
+                $_SESSION['userid'] = $loginUser->getId();
+                $_SESSION['useremail'] = $loginUser->getEmail();
+                header('location: /');
+            } else {
+                echo ('useremail or the password are wrong ! try again');
+                require("views/auth.php");
+            }
+        } else {
+            echo ('useremail or the password are wrong ! try again');
+            require("views/auth.php");
+        }
+
+        // dd($loginUser->getEmail());
+        // echo "User login process...";
     }
 
-    
+    public function logout(){
+        session_destroy();
+       
+        header('Location: /login');
+    }
+
     public function update()
     {
-         // CSRF
-         if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        // CSRF
+        if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
             die("CSRF token validation failed. Possible CSRF attack.");
         }
         echo "Updating user details...";
--- models/User.php
+++ models/User.php
@@ -1,47 +1,109 @@
-<?php 
+<?php
 
 class User
 {
- protected $username; 
- protected $useremail; 
- protected $userpassword; 
+   protected $id;
+   protected $username;
+   protected $useremail;
+   protected $userpassword;
 
-   public function __construct($username, $useremail, $userpassword)
+   // public function __construct($email,$db)
+   // {
+   //    $result = $db->query(
+   //       'select * from user where useremail = :email',
+   //       ['email' => $email]
+   //    );
+   //    $this->setName($result['username']);
+   //    $this->setEmail($result['useremail']);
+   //    $this->setPassword($result['userpassword']);
+   //    $this->setId($result['id']);
+   // }
+
+   public function __construct($db, $useremail = null, $username = null, $userpassword = null)
    {
-$this->setName($username);
-$this->setEmail($useremail);
-$this->setPassword($userpassword);
-}
+   if($userpassword == null){
+      $this->getUserbyEmail($db,$useremail);
+   }else{
+      $this->setName($username);
+      $this->setEmail($useremail);
+      $this->setPassword($userpassword);
+      $this->setId(null);}
+   }
 
    public static function isIndatabase($email, $db)
    {
 
       $result = $db->query(
          'select * from user where useremail = :email',
-         ['email'=>$email]
+         ['email' => $email]
       );
       return $result->rowCount();
-}
+   }
 
- //  SET
- public function setName($username){
-    $this->username = $username; 
- }
- public function setEmail($useremail){
-    $this->useremail = $useremail;
- }
- public function setPassword($userpassword){
-    $this->userpassword = $userpassword;
- }
+   //  SET
+   public function setName($username)
+   {
+      $this->username = $username;
+   }
+   public function setId($id)
+   {
+      $this->id = $id;
+   }
+   public function setEmail($useremail)
+   {
+      $this->useremail = $useremail;
+   }
+   public function setPassword($userpassword)
+   {
+      $this->userpassword = $userpassword;
+   }
 
-//  GET
- public function getName(){
-   return $this->username;
- }
- public function getEmail(){
-    return $this->userpassword;
-  }
-  public function getPassword(){
-    return $this->userpassword;
-  }
+   // add user to database 
+   public function createUser($db)
+   {
+       $db->query(
+         "insert into user (useremail,username,userpassword) values (:email, :name , :password)",
+         ['name' => $this->getName(), 'email' => $this->getEmail(), 'password' => $this->getPassword()]
+      );
+      $newUserId = $this->getIdByEmail($this->getEmail(),$db);
+      $this->setId($newUserId);
+      return $newUserId ;
+   }
+   //  GET
+   public static function getIdByEmail($email, $db)
+   {
+      return $db->query("select id from user where useremail = :email", ['email' => $email])->fetchColumn();
+   }
+   private function getUserbyEmail($db,$email){
+      $user = $db->query("select * from user where useremail = :email", ['email' => $email])->fetch();
+      // dd($user);
+      $this->setName($user['username']);
+      $this->setEmail($user['useremail']);
+      $this->setPassword($user['userpassword']);
+      $this->setId($user['id']);
+
+   }
+   public function getId()
+   {
+      return $this->id;
+   }
+   public function getName()
+   {
+      return $this->username;
+   }
+   public function getEmail()
+   {
+      return $this->useremail;
+   }
+   public function getPassword()
+   {
+      return $this->userpassword;
+   }
+
+
+   // password check
+   public function checkPassword($password)
+    {
+        return password_verify($password, $this->userpassword);
+    }
 }
